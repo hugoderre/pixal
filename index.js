f055a18a2029e648ab7936c7664fc51f7350f1b5
Patchook <hugo.d83@outlook.fr>
2021-05-04 21:33:41 +0200

    clean comments

--- index.js
+++ index.js
@@ -5,16 +5,11 @@ let y = 0;
 
 var keyState = {};
 
-// Keyboard input with customisable repeat (set to 0 for no key repeat)
-//
+
 function KeyboardController(keys, repeat) {
-    // Lookup of key codes to timer ID, or null for no repeat
-    //
+
     var timers = {};
 
-    // When key is pressed and we don't already think it's pressed, call the
-    // key action callback and set a timer to generate another one after a delay
-    //
     document.onkeydown = function (event) {
         console.log(event);
         var key = (event || window.event).keyCode;
@@ -29,8 +24,6 @@ function KeyboardController(keys, repeat) {
         return false;
     };
 
-    // Cancel timeout and mark key as released on keyup
-    //
     document.onkeyup = function (event) {
         var key = (event || window.event).keyCode;
         if (key in timers) {
@@ -40,9 +33,6 @@ function KeyboardController(keys, repeat) {
         }
     };
 
-    // When window is unfocused we may not get key events. To prevent this
-    // causing a key to 'get stuck down', cancel all held keys
-    //
     window.onblur = function () {
         for (key in timers)
             if (timers[key] !== null)
